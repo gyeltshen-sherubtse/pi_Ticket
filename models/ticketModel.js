9287ecb11fb4a55920a2ba3c79fca935eb3ee3d3
gwangchuk.sherubtse@rub.edu.bt <ugyeltongchuu98@gmail.com>
2025-03-07 16:32:05 +0600

    render one1

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -5,11 +5,13 @@ require("dotenv").config();
 
 // Database configuration
 const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, 
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
 });
 
 client.connect();
