9b65f12e3bcee2c4641cf47245837cb839e39b24
gyeltshen-aka <ugyeltongchuu98@gmail.com>
2025-03-07 14:44:10 +0600

    Initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const dotenv = require("dotenv")
 
 // Importing the controller functions
 const { generateQRCode, scanQRCode, scanQRCodePost, downloadTicket } = require('./controllers/ticketController');
